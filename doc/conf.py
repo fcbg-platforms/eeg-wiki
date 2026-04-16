8b37e4e21dcb3e5dbf56f58494ac26a47da62c28
gbirot <gwenael.birot@fcbg.ch>
2026-04-16 17:15:15 +0200

    CHANGE logo

--- doc/conf.py
+++ doc/conf.py
@@ -86,8 +86,8 @@ suppress_warnings = ["config.cache"]
 html_css_files = [
     "css/style.css",
 ]
-html_favicon = "_static/icons/FCBG.svg"
-html_logo = "_static/icons/FCBG.svg"
+html_favicon = "_static/icons/neuro@cb_v1.2.svg"
+html_logo = "_static/icons/neuro@cb_v1.2.svg"
 html_permalinks_icon = "🔗"
 html_show_sphinx = False
 html_static_path = ["_static"]
